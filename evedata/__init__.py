c34acd2378449d3127aa68b8b6983efac252aef9
Till Biskup <till.biskup@ptb.de>
2024-08-20 13:08:05 +0200

    Import facades in base package namespace

--- evedata/__init__.py
+++ evedata/__init__.py
@@ -3,3 +3,7 @@
 Importing synchrotron radiometry data conducted at BESSY/MLS in Berlin.
 
 """
+
+# Import facade classes
+from evedata.measurement.boundaries.measurement import Measurement  # noqa
+from evedata.evefile.boundaries.evefile import EveFile  # noqa
