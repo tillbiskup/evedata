8663fd5c99f1e70952f38289df7538b6719c46bf
Till Biskup <till@till-biskup.de>
2024-11-21 13:07:11 +0100

    Helper class displaying the scan module structure

--- evedata/evefile/controllers/info.py
+++ evedata/evefile/controllers/info.py
@@ -1,9 +1,19 @@
 """
-*Information on scans, often relevant for debugging purposes.*
+*Information on measurements, often relevant for debugging purposes.*
+
+Measurements contain often a lot of relevant information beyond the pure
+data, such as the scan module structure of the scan as recreated from the
+saved scan description (SCML file).
+
+
+Module documentation
+====================
 
 """
 
 import logging
+
+from evedata.evefile.entities import data
 
 
 logger = logging.getLogger(__name__)
@@ -11,48 +21,230 @@ logger = logging.getLogger(__name__)
 
 class ScanModuleStructure:
     """
-    One sentence (on one line) describing the class.
+    Collect basic information of the scan module structure of a scan.
 
-    More description comes here...
+    Operating on the :attr:`EveFile.scan_modules
+    <evedata.evefile.boundaries.evefile.EveFile.scan_modules>` attribute of
+    the :class:`EveFile <evedata.evefile.boundaries.evefile.EveFile>` class,
+    this class extracts and collects basic information on the scan modules the
+    given scan consists of. Hence, this class is basically aimed at
+    debugging purposes, or for providing a first overview of the scan
+    structure and the devices in each of the scan modules.
+
+    The (abbreviated) output showing the scan module structure of a real
+    measurement file may look like:
+
+    .. code-block:: none
+
+        + 1: Dynamic Axis Snapshot - #1 [1]
+        + 2: Dynamic Channel Snapshot - #1 [2]
+        + 3: Motor - #47 [3..555]
+            A Det-S (OMSMn:pi01700000)
+            A Det-P (OMSMn:pi01700001)
+            ...
+          + 4: Motor - #56 [4..272]
+              A SX700-Wavelength (nmEnerg:io1200000wl2e)
+              C Keysight_1 (A2980:23303chan1)
+
+    As you can see from this example, the output attempts to have minimal
+    markup, to allow to focus on the contents. For each of the scan modules
+    (marked by a ``+``), the ID, name, number of positions and first (and
+    last) position are extracted. For each of the devices of a scan module,
+    the type (A: axis, C: channel), the name and ID are extracted. The names
+    are those used in the GUI of the measurement program, while the IDs are
+    what you will find when opening an eveH5 file (*e.g.*, with the
+    ``hdfview`` program).
+
+    The resulting structure can be excessively configured, using the
+    respective attributes described below. However, usually the default
+    should be fine, and it is designed with a minimum markup in mind.
+
+    .. note::
+
+        Operating on the :attr:`EveFile.scan_modules
+        <evedata.evefile.boundaries.evefile.EveFile.scan_modules>` attribute of
+        the :class:`EveFile <evedata.evefile.boundaries.evefile.EveFile>`
+        class, the resulting scan structure may not in all details reflect
+        the original scan module structure, particularly given that the
+        special MPSKIP scans will usually be processed already. See the
+        :mod:`mpskip <evedata.evefile.controllers.mpskip>` module for details.
 
 
     Attributes
     ----------
-    attr : :class:`None`
-        Short description
+    scan_modules : :class:`dict`
+        Scan modules read from a given eveH5 file containing the SCML file.
+
+        Typically, this is the :attr:`EveFile.scan_modules
+        <evedata.evefile.boundaries.evefile.EveFile.scan_modules>` attribute of
+        the :class:`EveFile <evedata.evefile.boundaries.evefile.EveFile>`
+        class.
+
+    scan_module_marker : :class:`str`
+        Marker used to label a scan module.
+
+        Default: ``+``
+
+    axis_marker : :class:`str`
+        Marker used to label an axis contained in a scan module.
+
+        Default: ``A``
+
+    channel_marker : :class:`str`
+        Marker used to label a channel contained in a scan module.
+
+        Default: ``C``
+
+    indentation : :class:`str`
+        Indentation used to label nested scan modules (and their devices).
+
+        Default: two spaces
+
+    device_indentation : :class:`str`
+        Indentation used to label devices of scan modules.
+
+        Default: four spaces
+
+
+    Parameters
+    ----------
+    scan_modules : :class:`dict`
+        Scan modules read from a given eveH5 file containing the SCML file.
+
+        Typically, this is the :attr:`EveFile.scan_modules
+        <evedata.evefile.boundaries.evefile.EveFile.scan_modules>` attribute of
+        the :class:`EveFile <evedata.evefile.boundaries.evefile.EveFile>`
+        class.
+
 
     Raises
     ------
-    exception
-        Short description when and why raised
+    ValueError
+        Raised if no scan modules are available to operate on.
 
 
     Examples
     --------
-    It is always nice to give some examples how to use the class. Best to do
-    that with code examples:
+    Suppose you have loaded an eveH5 file using the :class:`EveFile
+    <evedata.evefile.boundaries.evefile.EveFile>` class, and the loaded eveH5
+    file contained the scan description (SCML file).
 
     .. code-block::
 
-        obj = ScanModuleStructure()
-        ...
+        eve_file = EveFile()
+        eve_file.load(filename="my_measurement_file.h5")
 
+        scan_module_structure = ScanModuleStructure(eve_file.scan_modules)
+        scan_module_structure.create()
 
+    This would create a textual overview of the scan structure in terms of
+    its scan modules and the devices contained in each of the scan modules,
+    and store this as a list of strings in the :attr:`structure` attribute.
+
+    However, usually you are interested in a straight output of the
+    structure, as you would like to debug something. Hence, simply replace
+    :meth:`create` with :meth:`print` and you are done:
+
+    .. code-block::
+
+        eve_file = EveFile()
+        eve_file.load(filename="my_measurement_file.h5")
+
+        scan_module_structure = ScanModuleStructure(eve_file.scan_modules)
+        scan_module_structure.print()
+
+    This will create an output similar to what is shown as an example above
+    at the beginning of the class documentation.
 
     """
 
-    def __init__(self):
-        self.scan_modules = {}
-        self.structure = []
+    def __init__(self, scan_modules=None):
+        if scan_modules is None:
+            scan_modules = {}
+        self.scan_modules = scan_modules
+
         self.scan_module_marker = "+"
+        self.axis_marker = "A"
+        self.channel_marker = "C"
+        self.indentation = "  "
+        self.device_indentation = "    "
+
+        self._structure = []
+        self._indentation_level = 0
+
+    @property
+    def structure(self):
+        """
+        Structure of the scan modules and their devices.
+
+        The structure is created using the :meth:`create` method, and it can
+        be plotted conveniently using the :meth:`print` method.
+
+        Returns
+        -------
+        structure : :class:`list`
+            Structure of the scan modules and their devices.
+
+            Each element is of type :class:`str`.
+
+        """
+        return self._structure
 
     def create(self):
+        """
+        Create the basic information of the scan module structure of a scan.
+
+        The resulting information is contained in the :attr:`structure`
+        attribute and can be printed conveniently using the :meth:`print`
+        method.
+        """
         if not self.scan_modules:
             raise ValueError("No scan modules")
         for scan_module in self.scan_modules.values():
-            self.structure.append(
-                f"{self.scan_module_marker} "
-                f"{scan_module.id}: {scan_module.name} - #"
-                f"{scan_module.positions[0]} [{scan_module.positions[0]}.."
-                f"{scan_module.positions[-1]}]"
+            self._create_scan_module_information(scan_module)
+            for device in scan_module.data.values():
+                self._create_device_information(device)
+            if scan_module.nested:
+                self._indentation_level += 1
+            elif not scan_module.appended:
+                self._indentation_level -= 1
+
+    def _create_scan_module_information(self, scan_module):
+        indentation = self.indentation * self._indentation_level
+        if len(scan_module.positions) > 1:
+            positions = (
+                f"{scan_module.positions[0]}..{scan_module.positions[-1]}"
             )
+        else:
+            positions = f"{scan_module.positions[0]}"
+        self._structure.append(
+            f"{indentation}{self.scan_module_marker} "
+            f"{scan_module.id}: {scan_module.name} - #"
+            f"{len(scan_module.positions)} [{positions}]"
+        )
+
+    def _create_device_information(self, device):
+        indentation = self.indentation * self._indentation_level
+        if isinstance(device, data.AxisData):
+            marker = self.axis_marker
+        elif isinstance(device, data.ChannelData):
+            marker = self.channel_marker
+        else:
+            marker = "?"
+        self._structure.append(
+            f"{indentation}{self.device_indentation}{marker} "
+            f"{device.metadata.name} ({device.metadata.id})"
+        )
+
+    def print(self):
+        """
+        Print the basic information of the scan module structure of a scan.
+
+        Convenience method to print the contents of the :attr:`structure`
+        attribute of the class line-by-line. Intern, the :meth:`create`
+        method is called first, to ensure the correct output being printed.
+
+        """
+        self.create()
+        for line in self._structure:
+            print(line)
--- tests/evefile/controllers/test_info.py
+++ tests/evefile/controllers/test_info.py
@@ -1,9 +1,11 @@
+import contextlib
+import io
 import unittest
 
 import numpy as np
 
 from evedata.evefile.controllers import info
-from evedata.evefile.entities.file import ScanModule
+from evedata.evefile.entities import file, data
 
 
 class TestScanModuleStructure(unittest.TestCase):
@@ -13,8 +15,19 @@ class TestScanModuleStructure(unittest.TestCase):
     def test_instantiate_class(self):
         pass
 
+    def test_instantiate_with_scan_modules_sets_scan_modules(self):
+        scan_module = file.ScanModule()
+        scan_module.id = 1
+        scan_module.name = "Diodes"
+        scan_module.positions = np.asarray([1])
+        scan_modules = {1: scan_module}
+        scan_module_structure = info.ScanModuleStructure(
+            scan_modules=scan_modules
+        )
+        self.assertTrue(scan_module_structure.scan_modules)
+
     def test_create_sets_structure(self):
-        scan_module = ScanModule()
+        scan_module = file.ScanModule()
         scan_module.id = 1
         scan_module.name = "Diodes"
         scan_module.positions = np.asarray([1])
@@ -28,10 +41,10 @@ class TestScanModuleStructure(unittest.TestCase):
             self.scan_module_structure.create()
 
     def test_create_sets_metadata_of_scan_module(self):
-        scan_module = ScanModule()
+        scan_module = file.ScanModule()
         scan_module.id = 1
         scan_module.name = "Diodes"
-        scan_module.positions = np.asarray([1])
+        scan_module.positions = np.asarray([1, 2, 3])
         self.scan_module_structure.scan_modules = {1: scan_module}
         self.scan_module_structure.create()
         self.assertIn(
@@ -42,14 +55,15 @@ class TestScanModuleStructure(unittest.TestCase):
         )
 
     def test_create_sets_metadata_of_scan_modules(self):
-        scan_module1 = ScanModule()
+        scan_module1 = file.ScanModule()
         scan_module1.id = 1
         scan_module1.name = "Diodes"
+        scan_module1.appended = 2
         scan_module1.positions = np.asarray([1, 2, 3])
         self.scan_module_structure.scan_modules[scan_module1.id] = (
             scan_module1
         )
-        scan_module2 = ScanModule()
+        scan_module2 = file.ScanModule()
         scan_module2.id = 2
         scan_module2.name = "MB"
         scan_module2.positions = np.asarray([4, 5, 6, 7, 8])
@@ -60,14 +74,210 @@ class TestScanModuleStructure(unittest.TestCase):
         self.assertIn(
             f"{self.scan_module_structure.scan_module_marker} "
             f"{scan_module1.id}: {scan_module1.name} - #"
-            f"{scan_module1.positions[0]} ["
+            f"{len(scan_module1.positions)} ["
             f"{scan_module1.positions[0]}..{scan_module1.positions[-1]}]",
             self.scan_module_structure.structure[0],
         )
         self.assertIn(
             f"{self.scan_module_structure.scan_module_marker} "
             f"{scan_module2.id}: {scan_module2.name} - #"
-            f"{scan_module2.positions[0]} ["
+            f"{len(scan_module2.positions)} ["
             f"{scan_module2.positions[0]}..{scan_module2.positions[-1]}]",
             self.scan_module_structure.structure[1],
+        )
+
+    def test_create_with_single_position_in_scan_module(self):
+        scan_module = file.ScanModule()
+        scan_module.id = 1
+        scan_module.name = "Diodes"
+        scan_module.positions = np.asarray([1])
+        self.scan_module_structure.scan_modules = {1: scan_module}
+        self.scan_module_structure.create()
+        self.assertIn(
+            f"{self.scan_module_structure.scan_module_marker} "
+            f"{scan_module.id}: {scan_module.name} - #"
+            f"{len(scan_module.positions)} [{scan_module.positions[0]}]",
+            self.scan_module_structure.structure[0],
+        )
+
+    def test_create_with_multiple_positions_in_scan_module(self):
+        scan_module = file.ScanModule()
+        scan_module.id = 1
+        scan_module.name = "Diodes"
+        scan_module.positions = np.asarray([1, 2, 3, 4])
+        self.scan_module_structure.scan_modules = {1: scan_module}
+        self.scan_module_structure.create()
+        self.assertIn(
+            f"{self.scan_module_structure.scan_module_marker} "
+            f"{scan_module.id}: {scan_module.name} - #"
+            f"{len(scan_module.positions)} [{scan_module.positions[0]}.."
+            f"{scan_module.positions[-1]}]",
+            self.scan_module_structure.structure[0],
+        )
+
+    def test_create_with_nested_scan_modules_adds_indentation(self):
+        scan_module1 = file.ScanModule()
+        scan_module1.id = 1
+        scan_module1.name = "Diodes"
+        scan_module1.nested = 2
+        scan_module1.positions = np.asarray([1, 2, 3])
+        self.scan_module_structure.scan_modules[scan_module1.id] = (
+            scan_module1
+        )
+        scan_module2 = file.ScanModule()
+        scan_module2.id = 2
+        scan_module2.name = "MB"
+        scan_module2.positions = np.asarray([4, 5, 6, 7, 8])
+        self.scan_module_structure.scan_modules[scan_module2.id] = (
+            scan_module2
+        )
+        self.scan_module_structure.create()
+        self.assertIn(
+            f"{self.scan_module_structure.scan_module_marker} "
+            f"{scan_module1.id}: {scan_module1.name} - #"
+            f"{len(scan_module1.positions)} ["
+            f"{scan_module1.positions[0]}..{scan_module1.positions[-1]}]",
+            self.scan_module_structure.structure[0],
+        )
+        self.assertIn(
+            f"{self.scan_module_structure.indentation}"
+            f"{self.scan_module_structure.scan_module_marker} "
+            f"{scan_module2.id}: {scan_module2.name} - #"
+            f"{len(scan_module2.positions)} ["
+            f"{scan_module2.positions[0]}..{scan_module2.positions[-1]}]",
+            self.scan_module_structure.structure[1],
+        )
+
+    def test_create_with_de_nested_scan_modules_removes_indentation(self):
+        scan_module1 = file.ScanModule()
+        scan_module1.id = 1
+        scan_module1.name = "Diodes"
+        scan_module1.nested = 2
+        scan_module1.appended = 3
+        scan_module1.positions = np.asarray([1, 2, 3])
+        self.scan_module_structure.scan_modules[scan_module1.id] = (
+            scan_module1
+        )
+        scan_module2 = file.ScanModule()
+        scan_module2.id = 2
+        scan_module2.name = "MB"
+        scan_module2.positions = np.asarray([4, 5, 6, 7, 8])
+        self.scan_module_structure.scan_modules[scan_module2.id] = (
+            scan_module2
+        )
+        scan_module3 = file.ScanModule()
+        scan_module3.id = 3
+        scan_module3.name = "DetX"
+        scan_module3.positions = np.asarray([9, 10])
+        self.scan_module_structure.scan_modules[scan_module3.id] = (
+            scan_module3
+        )
+        self.scan_module_structure.create()
+        self.assertIn(
+            f"{self.scan_module_structure.scan_module_marker} "
+            f"{scan_module1.id}: {scan_module1.name} - #"
+            f"{len(scan_module1.positions)} ["
+            f"{scan_module1.positions[0]}..{scan_module1.positions[-1]}]",
+            self.scan_module_structure.structure[0],
+        )
+        self.assertIn(
+            f"{self.scan_module_structure.indentation}"
+            f"{self.scan_module_structure.scan_module_marker} "
+            f"{scan_module2.id}: {scan_module2.name} - #"
+            f"{len(scan_module2.positions)} ["
+            f"{scan_module2.positions[0]}..{scan_module2.positions[-1]}]",
+            self.scan_module_structure.structure[1],
+        )
+        self.assertEqual(
+            f"{self.scan_module_structure.scan_module_marker} "
+            f"{scan_module3.id}: {scan_module3.name} - #"
+            f"{len(scan_module3.positions)} ["
+            f"{scan_module3.positions[0]}..{scan_module3.positions[-1]}]",
+            self.scan_module_structure.structure[2],
+        )
+
+    def test_create_with_axis_in_scan_module_adds_axis(self):
+        scan_module = file.ScanModule()
+        scan_module.id = 1
+        scan_module.name = "Diodes"
+        scan_module.positions = np.asarray([1])
+        axis = data.AxisData()
+        axis.metadata.id = "MotK7002:gw24807Col1scanner"
+        axis.metadata.name = "Matrix_input"
+        scan_module.data[axis.metadata.id] = axis
+        self.scan_module_structure.scan_modules = {1: scan_module}
+        self.scan_module_structure.create()
+        self.assertIn(
+            f"{self.scan_module_structure.device_indentation}"
+            f"{self.scan_module_structure.axis_marker} "
+            f"{axis.metadata.name} ({axis.metadata.id})",
+            self.scan_module_structure.structure[1],
+        )
+
+    def test_create_with_channel_in_scan_module_adds_channel(self):
+        scan_module = file.ScanModule()
+        scan_module.id = 1
+        scan_module.name = "Diodes"
+        scan_module.positions = np.asarray([1])
+        channel = data.ChannelData()
+        channel.metadata.id = "K0617:gw24825chan1"
+        channel.metadata.name = "Keithley_MSC"
+        scan_module.data[channel.metadata.id] = channel
+        self.scan_module_structure.scan_modules = {1: scan_module}
+        self.scan_module_structure.create()
+        self.assertIn(
+            f"{self.scan_module_structure.device_indentation}"
+            f"{self.scan_module_structure.channel_marker} "
+            f"{channel.metadata.name} ({channel.metadata.id})",
+            self.scan_module_structure.structure[1],
+        )
+
+    def test_create_with_axis_in_nested_scan_module_indents_axis(self):
+        scan_module1 = file.ScanModule()
+        scan_module1.id = 1
+        scan_module1.name = "Diodes"
+        scan_module1.nested = 2
+        scan_module1.positions = np.asarray([1, 2, 3])
+        self.scan_module_structure.scan_modules[scan_module1.id] = (
+            scan_module1
+        )
+        scan_module2 = file.ScanModule()
+        scan_module2.id = 2
+        scan_module2.name = "MB"
+        scan_module2.positions = np.asarray([4, 5, 6, 7, 8])
+        self.scan_module_structure.scan_modules[scan_module2.id] = (
+            scan_module2
+        )
+        axis = data.AxisData()
+        axis.metadata.id = "MotK7002:gw24807Col1scanner"
+        axis.metadata.name = "Matrix_input"
+        scan_module2.data[axis.metadata.id] = axis
+        self.scan_module_structure.create()
+        self.assertIn(
+            f"{self.scan_module_structure.indentation}"
+            f"{self.scan_module_structure.device_indentation}"
+            f"{self.scan_module_structure.axis_marker} "
+            f"{axis.metadata.name} ({axis.metadata.id})",
+            self.scan_module_structure.structure[2],
+        )
+
+    def test_structure_is_readonly(self):
+        with self.assertRaises(AttributeError):
+            self.scan_module_structure.structure = "Foo"  # noqa
+
+    def test_print_creates_correct_output(self):
+        scan_module = file.ScanModule()
+        scan_module.id = 1
+        scan_module.name = "Diodes"
+        scan_module.positions = np.asarray([1, 2, 3])
+        self.scan_module_structure.scan_modules = {1: scan_module}
+        output = io.StringIO()
+        with contextlib.redirect_stdout(output):
+            self.scan_module_structure.print()
+        self.assertIn(
+            f"{self.scan_module_structure.scan_module_marker} "
+            f"{scan_module.id}: {scan_module.name} - #"
+            f"{len(scan_module.positions)} ["
+            f"{scan_module.positions[0]}..{scan_module.positions[-1]}]",
+            output.getvalue(),
         )
